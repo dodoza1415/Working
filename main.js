bf85d67d67bebc23377bd09ab1bb8a26c182dc71
Surachet Pichaiwattanaporn <89179955+dodoza1415@users.noreply.github.com>
2022-08-29 20:19:49 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const changeColor = () => {
     return `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
 }
-
+console.log('Hello')
 document.getElementById("name").style.color = changeColor();
 document.getElementById("gen").style.color = changeColor();
 
